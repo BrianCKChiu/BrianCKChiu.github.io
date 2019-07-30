85728cb25ea871f8df26dc86b8b4ebae0727d593
Brian <brianchiu33@gmail.com>
2019-07-29 20:17:26 -0400

    add images and data

---
+++ chiubriA3/assets/data/myBooks.ts
@@ -0,0 +1,33 @@
+import { Book } from '../../app/books';
+
+export const BOOKDATA: Book[] = [
+ 
+    {
+        bName : "Harry Potter",
+        bAuthor : "J.K Rowling",
+        bGenre : "Magic",
+        bYear : 2009,
+        bPicture : "assets/images/img1.jpg"
+    },
+    {
+        bName : "History of War",
+        bAuthor : "King James VII",
+        bGenre : "War",
+        bYear : 1479,
+        bPicture : "assets/images/img2.png"
+    },
+    {
+        bName : "Fitness 2019",
+        bAuthor : "The Rock",
+        bGenre : "Life Style",
+        bYear : 2017,
+        bPicture : "assets/images/img3.jpg"
+    },
+    {
+        bName : "Intro to Web development",
+        bAuthor : "Wendi A.",
+        bGenre : "Tech",
+        bYear : 2019,
+        bPicture : "assets/images/img4.png"
+    }
+]
